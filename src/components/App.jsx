5ee463d6929f55a90b7f946036485d0988ade4ba
desi-coder11 <65103107+desi-coder11@users.noreply.github.com>
2022-12-31 00:34:50 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Card from "./Card";
+import contacts from "../contacts";
+import Avatar from "./Avatar";
+
+function App() {
+  return (
+    <div>
+      <h1 className="heading">My Contacts</h1>
+      <Avatar img="https://avatars.githubusercontent.com/u/65103107?v=4" />
+
+      <Card
+        name={contacts[0].name}
+        img={contacts[0].imgURL}
+        tel={contacts[0].phone}
+        email={contacts[0].email}
+      />
+      <Card
+        name={contacts[1].name}
+        img={contacts[1].imgURL}
+        tel={contacts[1].phone}
+        email={contacts[1].email}
+      />
+      <Card
+        name={contacts[2].name}
+        img={contacts[2].imgURL}
+        tel={contacts[2].phone}
+        email={contacts[2].email}
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Details.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Details(props) {
+  return <p className="info">{props.detailsInfo}</p>;
+}
+
+export default Details;
